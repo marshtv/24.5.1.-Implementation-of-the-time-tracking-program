e74655b7dea83bd6c1bfb1e1f6026e6a9ef32270
marshtv <marshtv.1972@gmail.com>
2023-01-02 21:34:04 +0300

    Complete

--- main.cpp
+++ main.cpp
@@ -4,67 +4,88 @@
 #include <string>
 #include <vector>
 
-std::time_t timeNow = std::time(nullptr);
-
-struct birth_data {
-	std::string name;
-	std::tm birth_date = *std::localtime(&timeNow);
+struct task_struct {
+	std::string title;
+	std::tm start;
+	std::tm end;
+	bool bStart = false;
+	bool bEnd = false;
 };
 
+void convert_sec(double sec) {
+	std::cout << "time spent ";
+	if (((int)sec / 3600) != 0)
+		std::cout << (int)sec / 3600 << "h ";
+	if ((((int)sec % 3600) / 60) != 0)
+		std::cout << ((int)sec % 3600) / 60 << "m ";
+	std::cout << ((int)sec % 3600) % 60 << "s" << std::endl;
+}
+
 int main() {
-	std::tm local = *std::localtime(&timeNow);
-	birth_data bd;
-	std::vector<birth_data> bd_cal;
-	std::vector<birth_data> nearest_cal;
+	std::vector<task_struct> task_vec;
+	std::string command;
 
-	std::cout << "Input name (or <end> for finish input):";
-	std::cin >> bd.name;
-	while (bd.name != "end") {
-		std::cout << "Input birth date (MM DD):";
-		std::cin >> std::get_time(&bd.birth_date, "%m/%d");
-		bd_cal.push_back(bd);
-
-		std::cout << "Input name (or <end> for finish input):";
-		std::cin >> bd.name;
-	}
-
-	for (int i = 0; i < bd_cal.size(); i++) {
-		if (bd_cal[i].birth_date.tm_mon < local.tm_mon)
-			bd_cal[i].birth_date.tm_year += 1;
-		else if (bd_cal[i].birth_date.tm_mon == local.tm_mon) {
-			if (bd_cal[i].birth_date.tm_mday < local.tm_mday)
-				bd_cal[i].birth_date.tm_year += 1;
+	std::cout << "You have few commands:" << std::endl;
+	std::cout << '"' << "begin" << '"' << "\t\tfor begin new task," << std::endl;
+	std::cout << '"' << "end" << '"' << "\t\tfor finish current task," << std::endl;
+	std::cout << '"' << "status" << '"' << "\tfor display info about all finished tasks," << std::endl;
+	std::cout << '"' << "exit" << '"' << "\t\tfor exit from program." << std::endl;
+	std::cout << "---------------------------------------------------------" << std::endl;
+	std::cout << "Input command:";
+	std::cin >> command;
+	while (command != "exit") {
+		if (command == "begin") {
+			if (!task_vec.empty()) {
+				int count_started = 0;
+				for (int i = 0; i < task_vec.size(); i++) {
+					if (task_vec[i].bStart) {
+						task_vec[i].bStart = false;
+						std::time_t timeNow = std::time(nullptr);
+						task_vec[i].end = *std::localtime(&timeNow);
+						task_vec[i].bEnd = true;
+						std::cout << "Task " << '"' << task_vec[i].title << '"' << " is ended" << std::endl;
+					}
+				}
+			}
+			task_struct task;
+			std::string in_str;
+			std::cout << "Input title of new task:";
+			std::getline(std::cin, in_str) >> task.title;
+			std::time_t timeNow = std::time(nullptr);
+			task.start = *std::localtime(&timeNow);
+			task.bStart = true;
+			task_vec.push_back(task);
+		} else if (command == "end") {
+			if (!task_vec.empty()) {
+				for (int i = 0; i < task_vec.size(); i++) {
+					if (task_vec[i].bStart) {
+						task_vec[i].bStart = false;
+						std::time_t timeNow = std::time(nullptr);
+						task_vec[i].end = *std::localtime(&timeNow);
+						task_vec[i].bEnd = true;
+						std::cout << "Task " << '"' << task_vec[i].title << '"' << " is ended" << std::endl;
+					}
+				}
+			} else
+				std::cout << "No tasks started yet!" << std::endl;
+		} else if (command == "status") {
+			if (!task_vec.empty()) {
+				double diff_sec;
+				double diff_sec_total = 0;
+				for (int i = 0; i < task_vec.size(); i++) {
+					if (task_vec[i].bEnd) {
+						diff_sec = difftime(std::mktime(&task_vec[i].end),
+											 std::mktime(&task_vec[i].start));
+						std::cout << i << ". Task " << '"' << task_vec[i].title << '"' << std::endl;
+						convert_sec(diff_sec);
+					}
+				}
+			} else
+				std::cout << "No tasks started yet!" << std::endl;
 		}
+		std::cout << "-----------------------------------" << std::endl;
+		std::cout << "Input command:";
+		std::cin >> command;
 	}
-
-	int nearest_index;
-	double diff_t = 1674302778;
-	for (int i = 0; i < bd_cal.size(); i++) {
-		std::time_t bd_t = std::mktime(&bd_cal[i].birth_date);
-		if (difftime(bd_t, timeNow) < diff_t) {
-			diff_t = difftime(bd_t, timeNow);
-			nearest_index = i;
-		}
-	}
-
-	std::cout << "------------------" << std::endl;
-	std::cout << "Nearest birthdays:" << std::endl;
-	std::cout << bd_cal[nearest_index].name << ":\t\t";
-	std::cout << std::put_time(&bd_cal[nearest_index].birth_date, "%d %B %Y") << std::endl;
-
-	std::vector<birth_data> nearest;
-	std::time_t nearest_t = std::mktime(&bd_cal[nearest_index].birth_date);
-	for (int i = 0; i < bd_cal.size(); i++) {
-		std::time_t bd_t = std::mktime(&bd_cal[i].birth_date);
-		if (i != nearest_index) {
-			if (difftime(bd_t, nearest_t) == 0)
-				nearest.push_back(bd_cal[i]);
-		}
-	}
-	if (!(nearest.empty())) {
-		for (int i = 0; i < nearest.size(); i++) {
-			std::cout << nearest[i].name << ":\t\t";
-			std::cout << std::put_time(&nearest[i].birth_date, "%d %B %Y") << std::endl;
-		}
-	}
+	return 0;
 }
